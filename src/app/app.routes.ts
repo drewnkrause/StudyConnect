5db2d6b0a88f3c3d4ae9c310e3c6643b71b7c721
Texas-Toasty <reecedparry@gmail.com>
2026-04-24 12:25:09 -0500

    Added base pages and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Registration } from './pages/registration/registration';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Group } from './pages/group/group';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Dashboard },
+    {path: 'register', component: Registration },
+    {path: 'login', component: Login },
+    {path: 'groups', component: Group }
+];
