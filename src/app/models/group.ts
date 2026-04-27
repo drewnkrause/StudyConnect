daaeabcde62ee3733f9f433c4f4911385a943d99
Drew Krause <drewkrause2@gmail.com>
2026-04-27 18:07:56 -0500

    Create data models

---
+++ src/app/models/group.ts
@@ -0,0 +1,14 @@
+import type { FieldValue, Timestamp } from 'firebase/firestore';
+
+export interface Group {
+  id: string;
+  title: string;
+  description: string;
+  courseId: string;
+  ownerId: string;
+  members: string[];
+  isPrivate: boolean;
+  type: 'student' | 'professor';
+  department: string;
+  lastActivity: Timestamp | FieldValue;
+}
---
+++ src/app/models/resource.ts
@@ -0,0 +1,1 @@
+import type { FieldValue, Timestamp } from 'firebase/firestore';
